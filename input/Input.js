4433b9a23a04162f3af14218beb0efc7d2ef509a
Sineka-M <160636123+Sineka-M@users.noreply.github.com>
2024-03-26 12:00:29 +0530

    Add files via upload

---
+++ input/Input.js
@@ -0,0 +1,21 @@
+document.getElementById("basicInput").addEventListener("input", function() {
+    console.log("Basic input changed:", this.value);
+  });
+  
+  document.getElementById("styledInput").addEventListener("input", function() {
+    console.log("Styled input changed:", this.value);
+  });
+  
+  document.getElementById("emailInput").addEventListener("input", function() {
+    console.log("Email input changed:", this.value);
+  });
+  
+  document.getElementById("passwordInput").addEventListener("input", function() {
+    console.log("Password input changed:", this.value);
+  });
+  
+  document.getElementById("numberInput").addEventListener("input", function() {
+    console.log("Number input changed:", this.value);
+  });
+  
+  
